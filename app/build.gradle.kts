34dee25ddd7a2c1bf6de3ed85d44494ec628e749
kiraa46 <21081010143@student.upnjatim.ac.id>
2023-12-12 22:27:35 +0700

    home-revise HomeScreen.kt and add FavoriteScreen.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.10.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation ("androidx.compose.ui:ui")
-    implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.compose.material3:material3:1.1.2")
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
